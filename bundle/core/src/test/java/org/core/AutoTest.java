838a04832139b404f3bb87626185f1d711878c9a
SchmidtBence <schmidt.bence@student.uni-miskolc.hu>
2021-10-06 08:51:31 +0200

    Test fájl létrehozva

---
+++ bundle/core/src/test/java/org/core/AutoTest.java
@@ -0,0 +1,13 @@
+package org.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutoTest {
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
